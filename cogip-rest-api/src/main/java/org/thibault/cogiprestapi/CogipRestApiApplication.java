beebb780d98a58af586a3e8a8c26db6d1deea59d
Thibault-be <thibault.dhooghe@gmail.com>
2024-02-19 12:09:05 +0100

    Set up spring initializr.

---
+++ cogip-rest-api/src/main/java/org/thibault/cogiprestapi/CogipRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.thibault.cogiprestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogipRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogipRestApiApplication.class, args);
+	}
+
+}
