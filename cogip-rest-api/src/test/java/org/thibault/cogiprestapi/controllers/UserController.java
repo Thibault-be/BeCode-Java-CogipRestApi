3b252a331a523fbcaca1287ce3a285bb28952ecd
Thibault-be <thibault.dhooghe@gmail.com>
2024-02-20 15:14:14 +0100

    Add User class and UserController with simple test method.

--- cogip-rest-api/src/test/java/org/thibault/cogiprestapi/controllers/UserController.java
+++ cogip-rest-api/src/test/java/org/thibault/cogiprestapi/controllers/UserController.java
@@ -1,17 +1,20 @@
 package org.thibault.cogiprestapi.controllers;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping
-public class CompanyController {
-
-
-
-
-
-
+public class UserController {
+  
+ 
+  @GetMapping ("/users")
+  public String getAllUsers(){
+    return "returning all users";
+  }
+  
+  
 }
