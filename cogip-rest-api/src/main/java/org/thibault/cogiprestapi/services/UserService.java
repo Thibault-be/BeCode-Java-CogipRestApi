0b6b30a622e0ac4a4b7391ef65c91b524370b10e
Thibault-be <thibault.dhooghe@gmail.com>
2024-02-20 16:47:56 +0100

    Start implementation of UserService

---
+++ cogip-rest-api/src/main/java/org/thibault/cogiprestapi/services/UserService.java
@@ -0,0 +1,12 @@
+package org.thibault.cogiprestapi.services;
+
+
+import org.springframework.stereotype.Service;
+import org.thibault.cogiprestapi.repositories.UserRepository;
+
+@Service
+public class UserService {
+  
+  private UserRepository userRepository;
+  
+}
