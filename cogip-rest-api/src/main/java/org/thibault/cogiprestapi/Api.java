5985f328253db47b6f71fe16c61e23d99bd63568
Thibault-be <thibault.dhooghe@gmail.com>
2024-02-20 14:22:58 +0100

    Add controller package and first controller, CompanyController.

--- cogip-rest-api/src/main/java/org/thibault/cogiprestapi/Api.java
+++ cogip-rest-api/src/main/java/org/thibault/cogiprestapi/Api.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CogipRestApiApplication {
+public class Api {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CogipRestApiApplication.class, args);
+		SpringApplication.run(Api.class, args);
 	}
 
 }
---
+++ cogip-rest-api/src/test/java/org/thibault/cogiprestapi/controllers/CompanyController.java
@@ -0,0 +1,14 @@
+package org.thibault.cogiprestapi.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CompanyController {
+
+
+
+
+}
