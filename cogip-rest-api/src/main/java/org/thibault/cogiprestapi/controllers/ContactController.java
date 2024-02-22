844f832e2537b69fc7d6d9c78a3250267ea25334
Thibault-be <thibault.dhooghe@gmail.com>
2024-02-22 12:20:25 +0100

    Start implementation in controller for getContactsByFilters. Service and repo left to be done.

--- cogip-rest-api/src/main/java/org/thibault/cogiprestapi/controllers/ContactController.java
+++ cogip-rest-api/src/main/java/org/thibault/cogiprestapi/controllers/ContactController.java
@@ -2,6 +2,7 @@ package org.thibault.cogiprestapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.thibault.cogiprestapi.model.Contact;
 import org.thibault.cogiprestapi.services.ContactService;
@@ -22,4 +23,15 @@ public class ContactController {
   public List<Contact> getAllContact() {
     return this.contactService.getAllContacts();
   }
+  
+  @GetMapping ("/contacts/search/{id}")
+  public List<Contact> getContactsByFilters(
+                          @RequestParam (required = false) String firstname,
+                          @RequestParam (required = false) String lastname,
+                          @RequestParam (required = false) String phone,
+                          @RequestParam (required = false) int companyId
+  ){
+    return this.contactService.getContactsByFilters(firstname, lastname, phone, companyId);
+  }
+  
 }
